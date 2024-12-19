930dba712e815a34874e5a3117bfbb9354eaf18d
JAEHYEON-BAE <154305251+JAEHYEON-BAE@users.noreply.github.com>
2024-12-19 11:08:03 +0000

    241219_2007(sidebar mask, design, logic)

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,33 +3,47 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
-  /** Header Menu Trigger */
   const menuIcon = document.querySelector('.menu-icon');
   const close = document.querySelector('.close');
-  const trigger = document.querySelector('.trigger');
+  const trigger = document.querySelector('.trigger');    // trigger == sidebar
   const menu_icon_svg = document.querySelector('.menu-icon > svg');
-  const pageContent = document.querySelector('.page-content');
+  // const pageContent = document.querySelector('.page-content');
+  const mask = document.querySelector('.mask');
 
+  /** Header Menu Trigger */
   let opened = false;
   
   menuIcon.addEventListener('click', function() {
     trigger.style.transform = 'translateX(0)';
-    pageContent.classList.add('blur');
     opened = true;
+    mask.style.pointerEvents = 'auto';
+    mask.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
+    mask.style.backdropFilter = 'blur(2px)';
   });
   close.addEventListener('click', function() {
     trigger.style.transform = 'translateX(100%)';
-    pageContent.classList.remove('blur');
     opened = false;
+    mask.style.pointerEvents = 'none';
+    mask.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+    mask.style.backdropFilter = 'none';
   });
-
+  mask.addEventListener('click', function() {
+    trigger.style.transform = 'translateX(100%)';
+    opened = false;
+    mask.style.pointerEvents = 'none';
+    mask.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+    mask.style.backdropFilter = 'none';
+  });
 
   /** Header Menu Trigger: interact with window width */
   const onPalm = 600;
   function triggerOnPalm() {
     if (opened && window.innerWidth > onPalm) {
       trigger.style.transform = 'translateX(100%)';
-      pageContent.classList.remove('blur');
+      opened = false;
+      mask.style.pointerEvents = 'none';
+      mask.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+      mask.style.backdropFilter = 'none';
     }
   }
   window.addEventListener('resize', triggerOnPalm);
